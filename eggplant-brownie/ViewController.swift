7366b0b1c776c61c9ac3ddfdabe146da9d7a7fff
Gabriel Biancardi <g.bbiancardi@gmail.com>
2023-01-06 19:18:16 -0300

    First commit

--- eggplant-brownie/ViewController.swift
+++ eggplant-brownie/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var felicidadeTextField: UITextField!
+    
+    @IBAction func adicionar() {
+        // var nome: String = "churros" não é necessário explicitar o tipo da variável
+        // let nome = "churros" torna o parâmetro uma constante
+        let nome = nomeTextField.text
+        let felicidade = felicidadeTextField.text
+        
+        print("Comi: \(nome) e fiquei com felicidade: \(felicidade)") // interpolação de variáveis
     }
-
 
 }
 
